c9f784ec79053a708951465a10b849b2837294da
Nitish Kumar <vickyyadav5383@gmail.com>
2025-02-16 09:34:27 +0530

    Updated the skeleton UI

--- client/src/components/ui/skeleton.jsx
+++ client/src/components/ui/skeleton.jsx
@@ -1,10 +1,15 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Skeleton({
-  className,
-  ...props
-}) {
-  return (<div className={cn("animate-pulse rounded-md bg-muted", className)} {...props} />);
+function Skeleton({ className, ...props }) {
+  return (
+    <div
+      className={cn(
+        "animate-pulse rounded-xl bg-gradient-to-r from-neutral-800 to-neutral-700 shadow-lg",
+        className
+      )}
+      {...props}
+    />
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
