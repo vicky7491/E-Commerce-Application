bde49991294cc0e45645f5ed13ea1c3aafb99070
Nitish Kumar <vickyyadav5383@gmail.com>
2026-03-22 19:32:12 +0530

    Footer name updated in accounts page

--- client/src/pages/shopping-view/account.jsx
+++ client/src/pages/shopping-view/account.jsx
@@ -2,7 +2,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import accImg from "../../assets/account.jpg";
 import Address from "@/components/shopping-view/address";
 import ShoppingOrders from "@/components/shopping-view/orders";
-import Footer from "./footer";
+import Footer from "./Footer";
 
 function ShoppingAccount() {
   return (
